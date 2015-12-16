4e7490365111d8dca5f868c4f5ea060aa50d95f8
Trumpower, Josiah <josiah.trumpower@cengage.com>
2015-12-16 16:56:30 -0500

    added templates and code to support them

--- vagrant/restaurant/project.py
+++ vagrant/restaurant/project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -11,26 +11,30 @@ app = Flask(__name__)
 def restaurantMenus():
   restaurants = restaurant_dao.getRestaurants()
   output = ''
+  restaurants_arr = []
   for restaurant in restaurants:
-    items = restaurant_dao.getMenuItemsByRestaurant(restaurant.id)
-    output += '<a href="/restaurant/%s/">Restaurant <i>%s</i></a>' % (restaurant.id, restaurant.name)
-    output += '</br>'
-    output += '<a href="/restaurant/%s/menu-item/new">Create new Menu item for <i>%s</i></a>' % (restaurant.id, restaurant.name)
-    output += '</br>'
-    output += '</br>'
-    for item in items:
-      output += item.name
-      output += '</br>'
-      output += item.price
-      output += '</br>'
-      output += item.description
-      output += '</br>'
-      output += '<a href="/restaurant/%s/menu-item/%s/edit">Edit</a>' % (restaurant.id, item.id)
-      output += '</br>'
-      output += '<a href="/restaurant/%s/menu-item/%s/delete">Delete</a>' % (restaurant.id, item.id)
-      output += '</br>'
-      output += '</br>'
-  return output
+  	items = restaurant_dao.getMenuItemsByRestaurant(restaurant.id)
+  	setattr(restaurant, "items", items)
+  	restaurants_arr.append(restaurant)
+  	print restaurant.items
+    # output += '<a href="/restaurant/%s/">Restaurant <i>%s</i></a>' % (restaurant.id, restaurant.name)
+    # output += '</br>'
+    # output += '<a href="/restaurant/%s/menu-item/new">Create new Menu item for <i>%s</i></a>' % (restaurant.id, restaurant.name)
+    # output += '</br>'
+    # output += '</br>'
+    # for item in items:
+    #   output += item.name
+    #   output += '</br>'
+    #   output += item.price
+    #   output += '</br>'
+    #   output += item.description
+    #   output += '</br>'
+    #   output += '<a href="/restaurant/%s/menu-item/%s/edit">Edit</a>' % (restaurant.id, item.id)
+    #   output += '</br>'
+    #   output += '<a href="/restaurant/%s/menu-item/%s/delete">Delete</a>' % (restaurant.id, item.id)
+    #   output += '</br>'
+    #   output += '</br>'
+  return render_template('restaurants.html', restaurants=restaurants_arr)
 
 
 @app.route('/restaurant/<int:restaurant_id>/')
@@ -53,7 +57,7 @@ def restaurantMenu(restaurant_id):
     output += '<a href="/restaurant/%s/menu-item/%s/delete">Delete</a>' % (restaurant.id, i.id)
     output += '</br>'
     output += '</br>'
-  return output
+  return render_template('restaurant.html', restaurant=restaurant, items=items)
 
 
 # Task 1: Create route for newMenuItem function here
@@ -70,8 +74,10 @@ def newMenuItem(restaurant_id):
   output += '<input type="text" name="price" placeholder="Item price" />'
   output += '<br />'
   output += '<input type="text" name="description" placeholder="Item description" />'
+  output += '<br />'
+  output += '<input type="submit" value="submit" />'
   output += '</form>'
-  return output
+  return render_template('new-restaurant.html', restaurant=restaurant)
 
 # Task 2: Create route for editMenuItem function here
 
@@ -88,12 +94,14 @@ def editMenuItem(restaurant_id, menu_id):
   output += '<input type="text" name="price" placeholder="Item price" value="%s" />' % item.price
   output += '<br />'
   output += '<input type="text" name="description" placeholder="Item description" value="%s" />' % item.description
+  output += '<br />'
+  output += '<input type="submit" value="submit" />'
   output += '</form>'
-  return output
+  return render_template('edit-menu-item.html', restaurant=restaurant, items=item)
 
 # Task 3: Create a route for deleteMenuItem function here
 
-@app.route('/restaurant/<int:restaurant_id>/menu-item/<int:menu_id>/delete')
+@app.route('/restaurant/<int:restaurant_id>/menu-item/<int:menu_id>/delete') 
 def deleteMenuItem(restaurant_id, menu_id):
   restaurant = restaurant_dao.getRestaurant(restaurant_id)
   item = restaurant_dao.getMenuItemByRestaurant(restaurant_id, menu_id)
@@ -107,7 +115,7 @@ def deleteMenuItem(restaurant_id, menu_id):
   output += '<a href="/restaurants/%s/"><input type="button" value="No" /></a>' % restaurant.id
   output += '<input type="submit" value="Yes" />'
   output += '</form>'
-  return output
+  return render_template('delete-menu-item.html', restaurant=restaurant, items=items)
 
 if __name__ == '__main__':
     app.debug = True
